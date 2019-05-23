8b993d85ecae089271edca8e12f72cfc40070016
Ali Selcuk <maselcuk@gmail.com>
2019-05-23 23:49:11 +0300

    Bump superv-js

--- js/src/Acp.vue
+++ js/src/Acp.vue
@@ -1,36 +1,50 @@
 <template>
 
   <layout-default router-type="tabbed">
+
+    <template slot="site-header">
+      <sv-site-header>
+        <template slot="logo">
+          <svg class="fill-current w-24" viewBox="0 0 226 62" xmlns="http://www.w3.org/2000/svg">-->
+                        <g fill="none" fill-rule="evenodd" font-weight="500" stroke="none" stroke-width="1">
+                          <g fill="#999999" transform="translate(-1.000000, -25.000000)">
+                            <text font-family="Montserrat" font-size="60">
+                              <tspan x="0" y="71">supreme</tspan>
+                            </text>
+                            <text fill="#67c23a" font-family=""
+                                  font-size="70" font-style="italic">
+                              <tspan x="156" y="72">E</tspan>
+                            </text>
+                          </g>
+                        </g>
+                      </svg>
+        </template>
+      </sv-site-header>
+    </template>
+
     <template slot="sidebar">
       <sv-sidebar>
-        <template slot="logo">
 
-          <svg class="fill-current w-24" viewBox="0 0 226 62" xmlns="http://www.w3.org/2000/svg">
-            <g fill="none" fill-rule="evenodd" font-weight="500" stroke="none" stroke-width="1">
-              <g fill="#999999" transform="translate(-1.000000, -25.000000)">
-                <text font-family="Montserrat" font-size="60">
-                  <tspan x="0" y="71">super</tspan>
-                </text>
-                <text fill="#67c23a" font-family=""
-                      font-size="70" font-style="italic">
-                  <tspan x="156" y="72">V</tspan>
-                </text>
-              </g>
-            </g>
-          </svg>
-        </template>
       </sv-sidebar>
     </template>
+<!--    <template slot="site-header">-->
+<!--      <sv-site-header>-->
+<!--        <template slot="logo">-->
+<!--&lt;!&ndash;-->
+<!--        </template>-->
+<!--      </sv-site-header>-->
+<!--    </template>-->
+
   </layout-default>
 
 </template>
 
 <script>
-import { LayoutDefault, SvSidebar } from 'superv-js'
+import { LayoutDefault, SvSidebar, SvSiteHeader } from 'superv-js'
 
 export default {
   name: 'Acp',
-  components: { SvSidebar, LayoutDefault },
+  components: { SvSiteHeader, SvSidebar, LayoutDefault },
 }
 </script>
 
