05edbb3849cf536a8bcf328e27aed89f63a3e962
Ali Selcuk <maselcuk@gmail.com>
2019-07-07 22:32:03 +0300

    Compile assets

--- js/src/main.js
+++ js/src/main.js
@@ -3,7 +3,6 @@ import supervjs from 'superv-js'
 import Acp from './Acp'
 import routes from './app/routes'
 
-
 let config
 try {
   config = JSON.parse(document.getElementById('config').innerHTML)
@@ -13,23 +12,22 @@ try {
 
 Vue.config.productionTip = false
 Vue.use(supervjs, {
-    config: {
-      name: process.env.VUE_APP_NAME,
-      apiUrl: config.apiUrl,
-      baseUrl: config.baseUrl
-    },
-    routes,
-    modules: []
-  }
-)
+  config: {
+    name: process.env.VUE_APP_NAME,
+    apiUrl: config.apiUrl,
+    baseUrl: config.baseUrl,
+  },
+  routes,
+  modules: [],
+})
 
 new Vue({
   el: '#app',
   name: 'root',
   data() {
     return {
-      layouts: { default: Acp }
+      layouts: { default: Acp },
     }
   },
-  mixins: [require('superv-js').LayoutMixin]
+  mixins: [require('superv-js').LayoutMixin],
 })
