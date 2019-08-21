1eee50b0ed4ef413559ac5ea6237714ac7e30083
Ali Selcuk <maselcuk@gmail.com>
2019-08-21 16:11:08 +0300

    Code style

--- js/src/Acp.vue
+++ js/src/Acp.vue
@@ -1,41 +1,31 @@
 <template>
 
-  <layout-default router-type="tabbed">
+    <layout-default router-type="tabbed">
 
-    <template slot="site-header">
-      <sv-site-header>
-        <template slot="logo">
-            <svg class="fill-current w-32" viewBox="0 0 300 62" xmlns="http://www.w3.org/2000/svg">-->
+        <template slot="site-header">
+            <sv-site-header>
+                <template slot="logo">
+                    <svg class="fill-current w-32" viewBox="0 0 300 62" xmlns="http://www.w3.org/2000/svg">-->
                         <g fill="none" fill-rule="evenodd" font-weight="500" stroke="none" stroke-width="1">
-                          <g fill="#999999" transform="translate(-1.000000, -25.000000)">
-                            <text font-family="Montserrat" font-size="60">
-                                <tspan x="0" y="71">Suprem</tspan>
-                            </text>
-                            <text fill="#67c23a" font-family=""
-                                  font-size="70" font-style="">
-                                <tspan x="216" y="64">e</tspan>
-                            </text>
-                          </g>
+                            <g fill="#999999" transform="translate(-1.000000, -25.000000)">
+                                <text font-family="Montserrat" font-size="60">
+                                    <tspan x="0" y="71">Suprem</tspan>
+                                </text>
+                                <text fill="#67c23a" font-family=""
+                                      font-size="70" font-style="">
+                                    <tspan x="216" y="64">e</tspan>
+                                </text>
+                            </g>
                         </g>
-                      </svg>
+                    </svg>
+                </template>
+            </sv-site-header>
         </template>
-      </sv-site-header>
-    </template>
 
-    <template slot="sidebar">
-      <sv-sidebar>
-
-      </sv-sidebar>
-    </template>
-<!--    <template slot="site-header">-->
-<!--      <sv-site-header>-->
-<!--        <template slot="logo">-->
-<!--&lt;!&ndash;-->
-<!--        </template>-->
-<!--      </sv-site-header>-->
-<!--    </template>-->
-
-  </layout-default>
+        <template slot="sidebar">
+            <sv-sidebar></sv-sidebar>
+        </template>
+    </layout-default>
 
 </template>
 
@@ -56,14 +46,12 @@ body {
   @apply bg-base;
 }
 
-
 .font-sans {
   font-size: 14px;
   font-family: 'Montserrat', 'Helvetica Neue', Arial, sans-serif;
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
 }
-
 
 .material-icons {
   font-size: 20px !important;
