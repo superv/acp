56fb5d1ebc2d6750ae2890979cd1dd6e4b4b5c2f
Ali Selcuk <maselcuk@gmail.com>
2019-09-28 01:53:06 +0300

    Minor fixes and transpile

--- resources/views/home.php
+++ resources/views/home.php
@@ -11,12 +11,12 @@
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700" rel=stylesheet>
   <title>SuperV Project</title>
   <link href=/vendor/superv/acp/js/chunk-2d0ce80b.2ef8ac52.js rel=prefetch>
-  <link href=/vendor/superv/acp/css/app.fb6febfa.css rel=preload as=style>
+  <link href=/vendor/superv/acp/css/app.0caa2569.css rel=preload as=style>
   <link href=/vendor/superv/acp/css/chunk-vendors.244017cc.css rel=preload as=style>
-  <link href=/vendor/superv/acp/js/app.7609b9a8.js rel=preload as=script>
+  <link href=/vendor/superv/acp/js/app.d4e13d16.js rel=preload as=script>
   <link href=/vendor/superv/acp/js/chunk-vendors.50aa8e25.js rel=preload as=script>
   <link href=/vendor/superv/acp/css/chunk-vendors.244017cc.css rel=stylesheet>
-  <link href=/vendor/superv/acp/css/app.fb6febfa.css rel=stylesheet>
+  <link href=/vendor/superv/acp/css/app.0caa2569.css rel=stylesheet>
 </head>
 <body class="font-sans font-normal antialiased text-smooth min-h-screen">
   <noscript><strong>We're sorry but frontend doesn't work at all without JavaScript enabled. Please enable it to
@@ -24,6 +24,6 @@
   <div id=app></div>
   <script id=config type=application/json><?php echo json_encode($config); ?></script>
   <script src=/vendor/superv/acp/js/chunk-vendors.50aa8e25.js></script>
-  <script src=/vendor/superv/acp/js/app.7609b9a8.js></script>
+  <script src=/vendor/superv/acp/js/app.d4e13d16.js></script>
 </body>
 </html>
